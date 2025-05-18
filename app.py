36ddef015df48ff0f2638b1a436d68be98989a59
Jonazhino <Jonazhino@DESKTOP-32LSEPT>
2025-05-18 16:05:32 -0500

    Updates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Desempeño del personal IE-RFA'
+    return render_template('index.html')
 
 #Iniciar el servicio web con Flask
 if __name__ == '__main__':

a73482b009039c850a35fc07dc93815689e3482c
Jonazhino <Jonazhino@DESKTOP-32LSEPT>
2025-05-19 11:09:40 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/login')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
+
+
 
 #Iniciar el servicio web con Flask
 if __name__ == '__main__':

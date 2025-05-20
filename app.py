91418aeca1ce015a24b8dc879e5ea901cd94ae6b
mccflurry <meencantas7u7@gmail.com>
2025-05-20 15:54:20 -0500

    tablas de la base de datos

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def home():
 
 #Iniciar el servicio web con Flask
 if __name__ == '__main__':
-    app.run(port=5000, debug=True, host='0.0.0.0')
+    app.run(port=8080, debug=True, host='0.0.0.0')

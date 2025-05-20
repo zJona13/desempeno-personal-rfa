c309ffad4ffe71ce7b6b55b7740fbefe0c094589
Jonazhino <Jonazhino@DESKTOP-32LSEPT>
2025-05-20 15:17:19 -0500

    Base datos

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     DB_HOST = '127.0.0.1'
-    DB_USER = 'root'
-    DB_PASSWORD = ''
-    DB_NAME = ''
+    DB_USER = 'if0_39036307'
+    DB_PASSWORD = 'I3LackStarxX12'
+    DB_NAME = 'if0_39036307_mecapro'
     DB_PORT = 3306
     SECRET_KEY = 'clave_secreta_para_jwt'

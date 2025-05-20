9ddca798e95b8c8a5a3b83eef9920a93ecd46f0d
mccflurry <meencantas7u7@gmail.com>
2025-05-20 17:03:39 -0500

    login y register

--- tools/security.py
+++ tools/security.py
@@ -1,6 +1,17 @@
-from argon2 import PasswordHasher
+# tools/security.py
+from argon2 import PasswordHasher, exceptions
 
 ph = PasswordHasher()
 
 def hash_password(password_plain):
     return ph.hash(password_plain)
+
+def verify_password(hashed_password, password_plain):
+    try:
+        return ph.verify(hashed_password, password_plain)
+    except exceptions.VerifyMismatchError:
+        return False
+    except Exception as e:
+        # Consider logging other exceptions
+        print(f"Error al verificar la contraseña: {e}")
+        return False
